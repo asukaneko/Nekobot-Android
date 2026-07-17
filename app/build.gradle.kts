b6f03071cda6aad863101975b8ba05a716f3092a
asukaneko <ycssbc@126.com>
2026-07-17 20:40:08 +0800

    docs(changelog): update for v0.2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.nekobot.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.2.5"
+        versionCode = 11
+        versionName = "0.2.6"
         vectorDrawables { useSupportLibrary = true }
     }
 
